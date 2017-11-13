1f24765d5bbd9e1df1c9f7363cf30bc29b7502fb
Sepehr Nezami <nezami@stanford.edu>
2017-11-13 04:15:48 -0800

    trying to isolate the title

--- Word Counter/WordCounter.m
+++ Word Counter/WordCounter.m
@@ -2,21 +2,24 @@ clear all
 close all
 clc
 
+%Year 2013 : <title> does not contain the title :'(
+for year=2007:2016 
+    file=fileread(strcat(int2str(year),'.html'));
+    title_starts=strfind(file,'<title>')+7;
+    title_ends  =strfind(file,'</title>')-1;
 
+    flag_title= title_not_confused(title_starts,title_ends);
 
-file=fileread('2007.html');
-title_starts=strfind(file,'<title>')+7;
-title_ends  =strfind(file,'</title>')-1;
-
-flag_title= title_not_confused(title_starts,title_ends);
-
-title=file(title_starts:title_ends);
-title=fix_string(title);
-title
-
+    title=file(title_starts:title_ends);
+    title=fix_string(title);
+    title
+end
 
 function str=fix_string(str)
-str=strrep(str,'’s','');
+str=strrep(str,'’s','((apostrophe s))');
+str=strrep(str,'’','((apostrophe at the end))');
+str=strrep(str,'‘','((apostrophe at the beginning))');
+
 end
 
 function flag_title=title_not_confused(title_starts,title_ends)
